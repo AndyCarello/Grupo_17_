62b770bc44194231070547063db9823d603207a2
Andrea Carello <andreacarello@hotmail.com>
2022-08-23 16:37:02 -0300

    Agregue mis rutas

--- index.js
+++ index.js
@@ -14,3 +14,23 @@ app.listen(80, () => {
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname, "/views/test.html"))
 });
+//Formulario de alta de usuario
+app.get("/registro", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/registro.html"))
+});
+//Agradecimiento por registrarse
+app.get("/registrado", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/registrado.html"))
+});
+//El correo ha sido validado
+app.get("/validado", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/validado.html"))
+});
+//Formulario de ingreso al sitio
+app.get("/ingreso", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/ingreso.html"))
+});
+//Muestra datos del perfil de usuario e historial de compras
+app.get("/perfil", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/perfil.html"))
+});
