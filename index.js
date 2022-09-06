9a8e31c944659c14610b5c468d748a5c2750dcf5
Andrea Carello <andreacarello@hotmail.com>
2022-09-06 15:27:59 -0300

    agregue en index.js ejs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require ("express"); 
 const app = express ();
 const path = require("path");
+app.set("view engine", "ejs");
 
 //defino la carpeta public para que el contenido se use directo
 app.use(express.static("public"));
