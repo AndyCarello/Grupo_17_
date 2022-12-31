7b13899aee0fbd139d188f91b20dd5db6e83351a
Andrea Carello <andreacarello@hotmail.com>
2022-12-30 22:21:41 -0300

    Crud ingredientes y categorias sin estilos

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,15 @@
+const db = require('../database/models');
+
 module.exports = {
 
     administrador: (req, res) => {
-        res.send("Listar usuarios")
+        res.render("admin/index", {
+            title: "Administrador del sitio",
+            estilos: [
+                "style.css",
+                "footer.css"        
+            ]
+        })
     },
     
     listarUsuarios: (req, res) => {
@@ -12,36 +20,110 @@ module.exports = {
         res.send("alternarPermisosAdmin")
     },
 
-    listarIngredientes: (req, res) =>{
-        res.send("listarIngredientes")
+    listarIngredientes: async (req, res) =>{
+        let ingredientes = await db.Ingredient.findAll(); //traigo todos los ingredientes de la db
+        res.render("admin/ingredientes/index", {
+            title: "Administrador de Ingredientes",
+            estilos: [
+                "style.css",
+                "footer.css"        
+            ],
+            ingredientes: ingredientes
+        })
     },
 
-    crearIngrediente: (req, res) =>{
-        res.send("crearIngrediente")
+    crearIngrediente: async (req, res) =>{
+        try {
+            let resultado = await db.Ingredient.create({
+                name: req.body.nombre
+            })
+        } catch(e) {
+            return res.send(e)
+        }
+
+        res.redirect("/admin/ingredientes")
     },
 
-    actualizarIngrediente: (req, res) =>{
-        res.send("actualizarIngrediente")
+    actualizarIngrediente: async (req, res) =>{
+        try {
+            let resultado = await db.Ingredient.update({
+                name: req.body.nombre
+            },
+            {
+                where: {id: req.params.id}
+            })
+        } catch(e) {
+            return res.send(e)
+        }
+        
+        res.redirect("/admin/ingredientes")
     },
 
-    eliminarIngrediente: (req, res) =>{
-        res.send("eliminarIngrediente")
+    eliminarIngrediente: async (req, res) =>{
+        try {
+            let resultado = await db.Ingredient.destroy({
+                where: {
+                    id: req.params.id
+                } 
+            })
+        } catch(e) {
+            return res.send(e)
+        }
+        
+        res.redirect("/admin/ingredientes")
     },
 
-    listarCategorias: (req, res) =>{
-        res.send("listarCategorias")
+    listarCategorias: async (req, res) =>{
+        let categorias = await db.Category.findAll(); //traigo todas las categorias de la db
+        res.render("admin/categorias/index", {
+            title: "Administrador de Categorias",
+            estilos: [
+                "style.css",
+                "footer.css"        
+            ],
+            categorias: categorias
+        })
     },
 
-    crearCategoria: (req, res) =>{
-        res.send("crearCategoria")
+    crearCategoria: async (req, res) =>{
+        try {
+            let resultado = await db.Category.create({
+                name: req.body.nombre
+            })
+        } catch(e) {
+            return res.send(e)
+        }
+        
+        res.redirect("/admin/categorias")
     },
 
-    actualizarCategoria: (req, res) =>{
-        res.send("actualizarCategoria")
+    actualizarCategoria: async (req, res) =>{
+        try {
+            let resultado = await db.Category.update({
+                name: req.body.nombre
+            },
+            {
+                where: {id: req.params.id}
+            })
+        } catch(e) {
+            return res.send(e)
+        }
+        
+        res.redirect("/admin/categorias")
     },
 
-    eliminarCategoria: (req, res) =>{
-        res.send("eliminarCategoria")
+    eliminarCategoria: async (req, res) =>{
+        try {
+            let resultado = await db.Category.destroy({
+                where: {
+                    id: req.params.id
+                } 
+            })
+        } catch(e) {
+            return res.send(e)
+        }
+        
+        res.redirect("/admin/categorias")
     },
 
     listarProductos: (req, res) =>{
