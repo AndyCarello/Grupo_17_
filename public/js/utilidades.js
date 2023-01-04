e25e1d5b78b9da78fb8c60bb372debca3c48c556
Andrea Carello <andreacarello@hotmail.com>
2023-01-04 03:58:58 -0300

    comentando modulo admin

--- public/js/utilidades.js
+++ public/js/utilidades.js
@@ -1,8 +1,12 @@
+/* //Toma el id de un div desde el data-objetivo de los 3 puntitos y
+    le pone o le saca la clase oculto */
 function mostrarDetalle(elemento) {
     let objetivo = document.getElementById(elemento.dataset.objetivo);
     objetivo.classList.toggle('oculto');
 }
 
+/* Busca todos los elementos con clase ocultable y les pone o saca la clase menu-oculto
+    la clase siempre-desktop evita que queden ocultos cuando esta en desktop */
 function ocultarMenu() {
     let elementos = document.getElementsByClassName("ocultable");
     for (elemento of elementos) {
